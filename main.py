0c35058ae9dfaca5370c91af34fb27a80c420877
14oleg88 <kelbeloleg@gmail.com>
2025-01-10 19:00:25 +0200

    image_editor

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ class Ui(QtWidgets.QMainWindow):
 class ImageEditor:  
     def __init__(self,):  
         self.original = None
-        self.image = None  
+        self.image = None 
+        self.history = [] 
+        self.history_index = -1
+        self.image_path = None
+        self.workdir = None
         self.temp_folder = tempfile.TemporaryDirectory()
         self.ui = Ui() 
         self.connects()
@@ -31,10 +35,16 @@ class ImageEditor:
         self.ui.folder_btn.clicked.connect(self.open_folder)  
         self.ui.open_folder.triggered.connect(self.open_folder)
         self.ui.open_file.triggered.connect(self.open_file)  
+        self.ui.rotate_left.clicked.connect(self.rotate_90)
+        self.ui.sharpen.triggered.connect(self.sharpen)
+        self.ui.blur.triggered.connect(self.blur)
         self.ui.image_list.currentRowChanged.connect(self.choose_image)
         self.ui.black_white.triggered.connect(self.do_black_white)
         self.ui.save.triggered.connect(self.save_file)
         self.ui.save_btn.clicked.connect(self.save_file)
+        self.ui.forward.clicked.connect(self.forward)
+        self.ui.back_btn.clicked.connect(self.back)
+        self.ui.del_btn.clicked.connect(self.delete_file)
 
     def get_images(self):
         self.folder_images = []
@@ -48,6 +58,7 @@ class ImageEditor:
         self.workdir = QFileDialog.getExistingDirectory()
         if self.workdir:
             self.get_images()
+            self.ui.image_list.clear()
             self.ui.image_list.addItems(self.folder_images)
 
     def open_file(self):  
@@ -59,7 +70,10 @@ class ImageEditor:
 
     def open(self, filename):  
         self.image = Image.open(filename)
-        self.original = self.image
+        self.original = self.image.copy()
+        self.history = [self.image.copy()]
+        self.history_index = 0
+        self.image_path = filename
 
     def choose_image(self):
         if self.ui.image_list.currentRow()>=0:
@@ -76,31 +90,83 @@ class ImageEditor:
         self.ui.current_image.setPixmap(pixmap)
         self.ui.current_image.show()
 
+    def add_to_history(self):
+        self.history.append(self.image.copy())
+        self.history_index += 1
+
     def temp_save(self):
-        temp_path = os.path.join(self.temp_folder.name, "temp_image.png")
+        temp_path = os.path.join(self.temp_folder.name, f"temp_image.png_{self.history_index}.png")
         self.image.save(temp_path)
         print("збережено в тимчасову папку")
         return temp_path
+
+    def delete_file(self):
+        if self.image_path:
+            check = QMessageBox.question(self.ui, "Видалення файлу", "Ви впевненні що хочете видалити фото?", QMessageBox.Yes|QMessageBox.No)
+        if check == QMessageBox.Yes:
+            os.remove(self.image_path)
+            if self.workdir:
+                    self.get_images()
+                    self.ui.image_list.clear()
+                    self.ui.image_list.addItems(self.folder_images)
+
+            self.ui.current_image.setPixmap(QPixmap())  
+            self.image = None  
+            self.original = None  
+            self.history = []  
+            self.history_index = -1  
+            self.image_path = None
+
 
     def save_file(self):
         if self.image:
             save_path, _ = QFileDialog.getSaveFileName(self.ui,"Виберіть фото", "", "Зображення (*.png *.jpg *.jpeg)")
             if save_path:
                 self.image.save(save_path)
+                if self.workdir:
+                    self.get_images()
+                    self.ui.image_list.clear()
+                    self.ui.image_list.addItems(self.folder_images)
             
 
-    def do_black_white(self):  
-        self.image = self.image.convert("L")  # перетворити на чорно-біле  
-        self.show_image(self.temp_save())
-  
-    def do_blur(self):  
-        self.image = self.image.filter(ImageFilter.BLUR)
+    def do_black_white(self): 
+        if self.image: 
+            self.image = self.image.convert("L")  # перетворити на чорно-біле  
+            self.add_to_history()
+            self.show_image(self.temp_save())
 
-    def rotate_90(self):  
-        self.image = self.image.transpose(Image.ROTATE_96) #поворот
+    
+
+  
+    def blur(self):
+        if self.image:  
+            self.image = self.image.filter(ImageFilter.BLUR)
+            self.add_to_history()
+            self.show_image(self.temp_save())
+
+    def rotate_90(self): 
+        if self.image:  
+            self.image = self.image.transpose(Image.ROTATE_90) #поворот
+            self.add_to_history()
+            self.show_image(self.temp_save())
+
+    def back(self):
+        if self.history_index > 0:
+            self.history_index -=1
+            self.image = self.history[self.history_index]
+            self.show_image(self.temp_save())
+
+    def forward(self):
+        if self.history_index < len(self.history) -1:
+            self.history_index +=1
+            self.image = self.history[self.history_index]
+            self.show_image(self.temp_save())
     
     def sharpen(self):
-        self.image = self.image.filter(ImageFilter.SHARPEN) #чіткість
+        if self.image: 
+            self.image = self.image.filter(ImageFilter.SHARPEN) #чіткість
+            self.add_to_history()
+            self.show_image(self.temp_save())
         
 
 app = QApplication([])  
